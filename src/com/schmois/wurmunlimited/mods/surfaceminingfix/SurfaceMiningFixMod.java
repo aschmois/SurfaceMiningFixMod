5abf0a478d6c4bc0dfa878c2aa71469d395a32ed
Andres Schmois <andresdroid@gmail.com>
2015-12-15 19:54:47 -0500

    Cleanup

--- src/com/schmois/wurmunlimited/mods/surfaceminingfix/SurfaceMiningFixMod.java
+++ src/com/schmois/wurmunlimited/mods/surfaceminingfix/SurfaceMiningFixMod.java
@@ -22,9 +22,6 @@ import org.gotti.wurmunlimited.modloader.interfaces.WurmMod;
 import org.gotti.wurmunlimited.modsupport.actions.ModActions;
 
 import com.schmois.wurmunlimited.mods.surfaceminingfix.items.AzbantiumPickaxe;
-import com.wurmonline.mesh.Tiles;
-import com.wurmonline.server.Items;
-import com.wurmonline.server.NoSuchItemException;
 import com.wurmonline.server.Server;
 import com.wurmonline.server.creatures.Creature;
 import com.wurmonline.server.deities.Deities;
@@ -44,7 +41,6 @@ import javassist.NotFoundException;
 import javassist.bytecode.BadBytecode;
 import javassist.bytecode.Bytecode;
 import javassist.bytecode.CodeAttribute;
-import javassist.bytecode.CodeIterator;
 import javassist.bytecode.Descriptor;
 import javassist.bytecode.LocalVariableAttribute;
 import javassist.bytecode.MethodInfo;
@@ -233,7 +229,7 @@ public class SurfaceMiningFixMod
         }
         if (Constants.addSeafloorMiningRigItem) {
             try {
-                replaceWaterHeightCondition();
+                SeafloorMiningRig.replaceWaterHeightCondition();
             } catch (NotFoundException | BadBytecode e) {
                 throw new HookException(e);
             }
@@ -344,36 +340,6 @@ public class SurfaceMiningFixMod
         return chance;
     }
 
-    public static boolean canMineUnderwater(Creature performer, Item item) {
-        final int digTilex = (int) performer.getStatus().getPositionX() + 2 >> 2;
-        final int digTiley = (int) performer.getStatus().getPositionY() + 2 >> 2;
-        int tile = Server.surfaceMesh.getTile(digTilex, digTiley);
-        int height = Tiles.decodeHeight(tile);
-        if (item.getTemplateId() == Constants.smr_id) {
-            if (height < -25) {
-                // TODO: Make SMR hollow
-                if (Constants.debug) {
-                    logger.log(Level.INFO, performer.getName() + " is mining with SMR.");
-                }
-                if (performer.getVehicle() != -10L) {
-                    try {
-                        final Item ivehic = Items.getItem(performer.getVehicle());
-                        if (ivehic.isBoat()) {
-                            return true;
-                        }
-                    } catch (NoSuchItemException ex3) {
-                    }
-                }
-                performer.getCommunicator()
-                        .sendNormalServerMessage("You must be on a boat of some kind to use the SMR.");
-            } else {
-                performer.getCommunicator().sendNormalServerMessage("The rock is too shallow to mine with an SMR.");
-            }
-            return false;
-        }
-        return height > -25;
-    }
-
     private void replaceMineSlopeCondition() throws NotFoundException, BadBytecode {
         ClassPool classPool = HookManager.getInstance().getClassPool();
         CtClass ctTileRockBehaviour = classPool.get("com.wurmonline.server.behaviours.TileRockBehaviour");
@@ -471,72 +437,4 @@ public class SurfaceMiningFixMod
         methodInfo.rebuildStackMap(classPool);
     }
 
-    /*  
-    @formatter:off
-         L728 
-        1475 iload 14;             h 
-        1477 bipush 231;
-        1479 if_icmple 1733;
-    @formatter:on
-    */
-    private void replaceWaterHeightCondition() throws NotFoundException, BadBytecode {
-        ClassPool classPool = HookManager.getInstance().getClassPool();
-        CtClass ctTileRockBehaviour = classPool.get("com.wurmonline.server.behaviours.TileRockBehaviour");
-        CtClass ctCreature = classPool.get("com.wurmonline.server.creatures.Creature");
-        CtClass ctItem = classPool.get("com.wurmonline.server.items.Item");
-        CtClass ctAction = classPool.get("com.wurmonline.server.behaviours.Action");
-
-        CtClass[] paramTypes = { ctAction, ctCreature, ctItem, CtPrimitiveType.intType, CtPrimitiveType.intType,
-                CtPrimitiveType.booleanType, CtPrimitiveType.intType, CtPrimitiveType.intType,
-                CtPrimitiveType.shortType, CtPrimitiveType.floatType };
-
-        CtMethod method = ctTileRockBehaviour.getMethod("action",
-                Descriptor.ofMethod(CtPrimitiveType.booleanType, paramTypes));
-
-        MethodInfo methodInfo = method.getMethodInfo();
-        CodeAttribute codeAttribute = methodInfo.getCodeAttribute();
-        LocalNameLookup localNames = new LocalNameLookup(
-                (LocalVariableAttribute) codeAttribute.getAttribute(LocalVariableAttribute.tag));
-
-        CodeIterator codeIterator = codeAttribute.iterator();
-
-        while (codeIterator.hasNext()) {
-            int pos = codeIterator.next();
-            int op = codeIterator.byteAt(pos);
-            if (op == CodeIterator.ISTORE) {
-                int fieldRefIdx = codeIterator.byteAt(pos + 1);
-                if (14 == fieldRefIdx) {
-                    Bytecode bytecode = new Bytecode(codeIterator.get().getConstPool());
-                    bytecode.addGap(1);
-                    codeIterator.insertAt(pos + 2, bytecode.get());
-                    break;
-                }
-            }
-        }
-
-        Bytecode bytecode = new Bytecode(methodInfo.getConstPool());
-
-        bytecode.add(Bytecode.NOP);
-        bytecode.addIload(14);
-        bytecode.add(Bytecode.BIPUSH);
-        bytecode.add(231);
-        bytecode.add(Bytecode.IF_ICMPLE);
-
-        byte[] search = bytecode.get();
-
-        logger.log(Level.INFO, "Search bytecode length: " + search.length);
-
-        bytecode = new Bytecode(methodInfo.getConstPool());
-        bytecode.addAload(localNames.get("performer"));
-        bytecode.addAload(localNames.get("source"));
-        bytecode.addInvokestatic(classPool.get(this.getClass().getName()), "canMineUnderwater",
-                Descriptor.ofMethod(CtPrimitiveType.booleanType, new CtClass[] { ctCreature, ctItem }));
-        bytecode.add(Bytecode.IFEQ);
-        byte[] replacement = bytecode.get();
-
-        logger.log(Level.INFO, "Replacement bytecode length: " + replacement.length);
-
-        new CodeReplacer(codeAttribute).replaceCode(search, replacement);
-        methodInfo.rebuildStackMap(classPool);
-    }
 }
--- src/com/wurmonline/server/items/SeafloorMiningRig.java
+++ src/com/wurmonline/server/items/SeafloorMiningRig.java
@@ -4,13 +4,34 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.gotti.wurmunlimited.modloader.classhooks.CodeReplacer;
+import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
+import org.gotti.wurmunlimited.modloader.classhooks.LocalNameLookup;
 import org.gotti.wurmunlimited.modsupport.ItemTemplateBuilder;
 
 import com.schmois.wurmunlimited.mods.surfaceminingfix.Constants;
+import com.wurmonline.mesh.Tiles;
+import com.wurmonline.server.Items;
 import com.wurmonline.server.MiscConstants;
+import com.wurmonline.server.NoSuchItemException;
+import com.wurmonline.server.Server;
+import com.wurmonline.server.creatures.Creature;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtPrimitiveType;
+import javassist.NotFoundException;
+import javassist.bytecode.BadBytecode;
+import javassist.bytecode.Bytecode;
+import javassist.bytecode.CodeAttribute;
+import javassist.bytecode.CodeIterator;
+import javassist.bytecode.Descriptor;
+import javassist.bytecode.LocalVariableAttribute;
+import javassist.bytecode.MethodInfo;
 
 public class SeafloorMiningRig {
-    Logger logger = Logger.getLogger(this.getClass().getName());
+    static Logger logger = Logger.getLogger(SeafloorMiningRig.class.getName());
 
     public SeafloorMiningRig() {
         logger.log(Level.INFO, "Azbantium Pickaxe enabled, creating item...");
@@ -58,6 +79,101 @@ public class SeafloorMiningRig {
             smr.addRequirement(leadReq);
         } else
             throw new Exception("ERROR ATTEMPTING TO CREATE ENTRY FOR " + id);
+    }
+
+    public static boolean canMineUnderwater(Creature performer, Item item) {
+        final int digTilex = (int) performer.getStatus().getPositionX() + 2 >> 2;
+        final int digTiley = (int) performer.getStatus().getPositionY() + 2 >> 2;
+        int tile = Server.surfaceMesh.getTile(digTilex, digTiley);
+        int height = Tiles.decodeHeight(tile);
+        if (item.getTemplateId() == Constants.smr_id) {
+            if (height < -25) {
+                // TODO: Make SMR hollow
+                if (Constants.debug) {
+                    logger.log(Level.INFO, performer.getName() + " is mining with SMR.");
+                }
+                if (performer.getVehicle() != -10L) {
+                    try {
+                        final Item ivehic = Items.getItem(performer.getVehicle());
+                        if (ivehic.isBoat()) {
+                            return true;
+                        }
+                    } catch (NoSuchItemException ex3) {
+                    }
+                }
+                performer.getCommunicator()
+                        .sendNormalServerMessage("You must be on a boat of some kind to use the SMR.");
+            } else {
+                performer.getCommunicator().sendNormalServerMessage("The rock is too shallow to mine with an SMR.");
+            }
+            return false;
+        }
+        return height > -25;
+    }
+
+    /*  
+    @formatter:off
+         L728 
+        1475 iload 14;             h 
+        1477 bipush 231;
+        1479 if_icmple 1733;
+    @formatter:on
+    */
+    public static void replaceWaterHeightCondition() throws NotFoundException, BadBytecode {
+        ClassPool classPool = HookManager.getInstance().getClassPool();
+        CtClass ctTileRockBehaviour = classPool.get("com.wurmonline.server.behaviours.TileRockBehaviour");
+        CtClass ctCreature = classPool.get("com.wurmonline.server.creatures.Creature");
+        CtClass ctItem = classPool.get("com.wurmonline.server.items.Item");
+        CtClass ctAction = classPool.get("com.wurmonline.server.behaviours.Action");
+
+        CtClass[] paramTypes = { ctAction, ctCreature, ctItem, CtPrimitiveType.intType, CtPrimitiveType.intType,
+                CtPrimitiveType.booleanType, CtPrimitiveType.intType, CtPrimitiveType.intType,
+                CtPrimitiveType.shortType, CtPrimitiveType.floatType };
+
+        CtMethod method = ctTileRockBehaviour.getMethod("action",
+                Descriptor.ofMethod(CtPrimitiveType.booleanType, paramTypes));
+
+        MethodInfo methodInfo = method.getMethodInfo();
+        CodeAttribute codeAttribute = methodInfo.getCodeAttribute();
+        LocalNameLookup localNames = new LocalNameLookup(
+                (LocalVariableAttribute) codeAttribute.getAttribute(LocalVariableAttribute.tag));
+
+        CodeIterator codeIterator = codeAttribute.iterator();
+
+        while (codeIterator.hasNext()) {
+            int pos = codeIterator.next();
+            int op = codeIterator.byteAt(pos);
+            if (op == CodeIterator.ISTORE) {
+                int fieldRefIdx = codeIterator.byteAt(pos + 1);
+                if (14 == fieldRefIdx) {
+                    Bytecode bytecode = new Bytecode(codeIterator.get().getConstPool());
+                    bytecode.addGap(1);
+                    codeIterator.insertAt(pos + 2, bytecode.get());
+                    break;
+                }
+            }
+        }
+
+        Bytecode bytecode = new Bytecode(methodInfo.getConstPool());
+
+        bytecode.add(Bytecode.NOP);
+        bytecode.addIload(14);
+        bytecode.add(Bytecode.BIPUSH);
+        bytecode.add(231);
+        bytecode.add(Bytecode.IF_ICMPLE);
+
+        byte[] search = bytecode.get();
+
+        bytecode = new Bytecode(methodInfo.getConstPool());
+        bytecode.addAload(localNames.get("performer"));
+        bytecode.addAload(localNames.get("source"));
+        bytecode.addInvokestatic(classPool.get(SeafloorMiningRig.class.getName()), "canMineUnderwater",
+                Descriptor.ofMethod(CtPrimitiveType.booleanType, new CtClass[] { ctCreature, ctItem }));
+        bytecode.add(Bytecode.IFEQ);
+        byte[] replacement = bytecode.get();
+
+        new CodeReplacer(codeAttribute).replaceCode(search, replacement);
+        methodInfo.rebuildStackMap(classPool);
     }
 
 }
